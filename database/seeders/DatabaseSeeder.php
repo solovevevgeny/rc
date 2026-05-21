33a75854f1cddcf0f42df5f925d5d0140ffda970
Solovev Evgeny <evgeny.solovev@gmail.com>
2026-05-21 12:15:50 +0300

    seeder for TrainType

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
 
         $this->call(EventSeeder::class);
+        
+        $this->call(TrainTypesSeeder::class);
     }
 }
