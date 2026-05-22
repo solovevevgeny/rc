3bf9ddf4513f3aaa15fb8015dc28b971acb12999
Solovev Evgeny <evgeny.solovev@gmail.com>
2026-05-22 10:47:50 +0300

    response with role

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
     }
 
     public function show($id) {
-        $user = User::findOrFail($id);
+        $user = User::with('role')->findOrFail($id);
         return $user;
     }
 
